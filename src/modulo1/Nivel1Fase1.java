1b69b4ec6649c496eb9131b4d731b41fd57dfea2
ariel <arielkeflix@gmail.com>
2021-02-16 11:59:07 +0100

    subo 1

---
+++ src/modulo1/Nivel1Fase1.java
@@ -0,0 +1,18 @@
+package modulo1;
+
+public class Nivel1Fase1 {
+
+	public static void main(String[] args) {
+		
+		String nombre = "Ariel";
+		String apellido1 = "Liffschitz";
+		String apellido2 = "Cohan";
+		
+		System.out.println("El nombre completo es: "+ nombre + " "+apellido1+" "+ apellido2);
+		System.out.println("El nombre completo es: "+ nombre + "/"+apellido1+"/"+ apellido2);
+		
+		
+
+	}
+
+}
